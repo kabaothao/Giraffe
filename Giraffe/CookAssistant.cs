9e463b17e0a66afa5d37dff4d78b49eb14d63fd6
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-02-04 09:32:42 -0600

    add cookassitant example

---
+++ Giraffe/CookAssistant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    internal class CookAssistant
+    {
+        public static void SayHi(string Name)
+        {
+            Console.WriteLine("Hello " + Name);
+        }
+    }
+}
+
+//you can create a bunch of these static methods and used to do different things. You can include static methods
